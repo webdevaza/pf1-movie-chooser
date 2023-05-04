ee8e507c2950d8b4741457da4f4f6073f6194910
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-05-04 11:08:58 +0600

    edited navbar (#8)
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,18 @@
 <!doctype html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite('resources/css/app.css')
-  <title>Movie-chooser</title>
-  <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ Vite::asset('resources/css/styles.css') }}">
-</head>
-<body class="text-gray-600 bg-gray-100 work-sans leading-normal text-base tracking-normal">
-  @include('partials.navbar')
-
-  @yield('content')
-  @include('partials.footer')
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Movie-chooser</title>
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:200,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/styles.css') }}">
+    <script src="{{ Vite::asset('resources/js/navbar.js') }}"></script>
+  </head>
+  <body class="text-gray-600 bg-gray-100 work-sans leading-normal text-base tracking-normal">
+    @include('partials.navbar')
+    @yield('content')
+    @include('partials.footer')
+  </body>
 </html>
